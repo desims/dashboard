27319dd842f06966c8c767e9d9e2637a76d20c2e
Desi Sitompul <sdesimayasari@gmail.com>
2024-11-13 09:07:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="YouTube Channel Dashboard", layout="wide")
 # Helper functions
 @st.cache_data
 def load_data():
-    data = pd.read_csv("youtube_channel_data.csv")
+    data = pd.read_csv("01 Call-Center-Dataset")
     data['DATE'] = pd.to_datetime(data['DATE'])
     data['NET_SUBSCRIBERS'] = data['SUBSCRIBERS_GAINED'] - data['SUBSCRIBERS_LOST']
     return data
